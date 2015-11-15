70b43883e45d1872ac25d659eafc2a4a823e94b0
Ugnius <ugnius.malukas@gmail.com>
2015-11-15 19:09:01 +0200

    Described graph.

--- main/app.go
+++ main/app.go
@@ -5,7 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var v0 Vertex = Vertex{name:"A"}
+	var v1 Vertex = Vertex{name:"B"}
+	var v2 Vertex = Vertex{name:"C"}
+	var v3 Vertex = Vertex{name:"D"}
+
+	v0.adjacencies = []Edge{Edge{&v1, 1}, Edge{&v2, 3}}
+	v1.adjacencies = []Edge{Edge{&v0, 2}, Edge{&v2, 1}}
+	v2.adjacencies = []Edge{Edge{&v3, 1}}
+	v3.adjacencies = []Edge{Edge{&v0, 3}}
+
+	vertices := []*Vertex{&v0, &v1, &v2, &v3}
+
+	for _, v := range vertices {
+		fmt.Println(v.name)
+	}
 }
 
 type Vertex struct {
