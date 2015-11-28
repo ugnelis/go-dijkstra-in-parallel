ad75e4844c0ad6bad71b018852eca101537fd89d
Ugnius <ugnius.malukas@gmail.com>
2015-11-28 16:17:25 +0200

    Added test for 20 vertices.

--- main/app_test.go
+++ main/app_test.go
@@ -194,6 +194,111 @@ func Example15Vertices() {
 	// Path: O
 }
 
+func Example20Vertices() {
+	var v0 Vertex = Vertex{name:"A"}
+	var v1 Vertex = Vertex{name:"B"}
+	var v2 Vertex = Vertex{name:"C"}
+	var v3 Vertex = Vertex{name:"D"}
+	var v4 Vertex = Vertex{name:"E"}
+	var v5 Vertex = Vertex{name:"F"}
+	var v6 Vertex = Vertex{name:"G"}
+	var v7 Vertex = Vertex{name:"H"}
+	var v8 Vertex = Vertex{name:"I"}
+	var v9 Vertex = Vertex{name:"J"}
+	var v10 Vertex = Vertex{name:"K"}
+	var v11 Vertex = Vertex{name:"L"}
+	var v12 Vertex = Vertex{name:"M"}
+	var v13 Vertex = Vertex{name:"N"}
+	var v14 Vertex = Vertex{name:"O"}
+	var v15 Vertex = Vertex{name:"P"}
+	var v16 Vertex = Vertex{name:"Q"}
+	var v17 Vertex = Vertex{name:"R"}
+	var v18 Vertex = Vertex{name:"S"}
+	var v19 Vertex = Vertex{name:"T"}
+
+	v0.adjacencies = []Edge{Edge{&v19, 8}}
+	v1.adjacencies = []Edge{Edge{&v0, 6}}
+	v2.adjacencies = []Edge{Edge{&v11, 3}}
+	v3.adjacencies = []Edge{Edge{&v17, 4}}
+	v4.adjacencies = []Edge{Edge{&v3, 3}}
+	v5.adjacencies = []Edge{Edge{&v18, 2}}
+	v6.adjacencies = []Edge{Edge{&v12, 3}}
+	v7.adjacencies = []Edge{Edge{&v13, 5}}
+	v8.adjacencies = []Edge{Edge{&v7, 5}, Edge{&v9, 3}}
+	v9.adjacencies = []Edge{Edge{&v7, 3}, Edge{&v16, 3}}
+	v10.adjacencies = []Edge{Edge{&v13, 7}}
+	v11.adjacencies = []Edge{Edge{&v12, 3}, Edge{&v1, 4}}
+	v12.adjacencies = []Edge{Edge{&v5, 2}, Edge{&v15, 4}}
+	v13.adjacencies = []Edge{Edge{&v11, 8}, Edge{&v6, 6}}
+	v14.adjacencies = []Edge{Edge{&v10, 6}}
+	v15.adjacencies = []Edge{Edge{&v4, 3}}
+	v16.adjacencies = []Edge{Edge{&v14, 5}}
+	v17.adjacencies = []Edge{Edge{&v2, 5}}
+	v18.adjacencies = []Edge{Edge{&v4, 4}}
+	v19.adjacencies = []Edge{Edge{&v8, 5}}
+
+	vertices := []*Vertex{&v0, &v1, &v2, &v3, &v4, &v5, &v6, &v7, &v8, &v9,
+		&v10, &v11, &v12, &v13, &v14, &v15, &v16, &v17, &v18, &v19}
+
+	for _, v := range vertices {
+		v.minDistance = math.MaxFloat64
+	}
+
+	ComputePaths(&v13)
+
+	for _, v := range vertices {
+		fmt.Print("Distance to ", v.name, ": ", v.minDistance, "\n")
+		path := GetShortestPathTo(v)
+		fmt.Print("Path: ")
+		for _, p := range path {
+			fmt.Print(p.name)
+		}
+		fmt.Print("\n")
+	}
+
+	// Output:
+	// Distance to A: 18
+	// Path: NLBA
+	// Distance to B: 12
+	// Path: NLB
+	// Distance to C: 28
+	// Path: NGMPEDRC
+	// Distance to D: 19
+	// Path: NGMPED
+	// Distance to E: 16
+	// Path: NGMPE
+	// Distance to F: 11
+	// Path: NGMF
+	// Distance to G: 6
+	// Path: NG
+	// Distance to H: 36
+	// Path: NLBATIH
+	// Distance to I: 31
+	// Path: NLBATI
+	// Distance to J: 34
+	// Path: NLBATIJ
+	// Distance to K: 48
+	// Path: NLBATIJQOK
+	// Distance to L: 8
+	// Path: NL
+	// Distance to M: 9
+	// Path: NGM
+	// Distance to N: 0
+	// Path: N
+	// Distance to O: 42
+	// Path: NLBATIJQO
+	// Distance to P: 13
+	// Path: NGMP
+	// Distance to Q: 37
+	// Path: NLBATIJQ
+	// Distance to R: 23
+	// Path: NGMPEDR
+	// Distance to S: 13
+	// Path: NGMFS
+	// Distance to T: 26
+	// Path: NLBAT
+}
+
 func Example26Vertices() {
 	var v0 Vertex = Vertex{name:"A"}
 	var v1 Vertex = Vertex{name:"B"}
